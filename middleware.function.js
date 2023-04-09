aa0a2189a641f56a155ed919a8a8df988688757c
RichardLeal <you@example.com>
2023-04-09 16:44:47 -0300

    Check if request is from user token

--- middleware.function.js
+++ middleware.function.js
@@ -4,18 +4,24 @@ const SECRET_KEY = process.env.JWT_SECRET || 'mysecretkey';
 function verifyRole(role) {
   return (req, res, next) => {
     // Middleware code
-    const regitration = req.body;
+    const regitrationBody = req.body;
+    const registrationParams = req.params;
+
     const token = req.headers.authorization;
     if (!token) {
       return res.status(401).send({ auth: false, message: 'No token provided.' });
     }
   
     jwt.verify(token, SECRET_KEY, (err, decoded) => {
-      console.log('decoded', decoded);
       if (err)
         return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
 
       if(decoded.role !== role)
+        return res.status(401).send('Unauthorized');
+
+      // Check if request is from user token
+      if((!!regitrationBody.registration && decoded.registration !== regitrationBody.registration) 
+      || (!!registrationParams.registration && decoded.registration !== registrationParams?.registration))
         return res.status(401).send('Unauthorized');
 
       // if everything good, save to request for use in other routes
@@ -33,7 +39,6 @@ function verifyToken(req, res, next) {
   }
 
   jwt.verify(token, SECRET_KEY, (err, decoded) => {
-    console.log('decoded', decoded);
     if (err) {
       return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
     }
